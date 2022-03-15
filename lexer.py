00a19efbd6c1299f387a2a1d39d6d21ec6492397
Ernesto Casco <ecasco@fb.com>
2022-03-14 18:59:55 -0600

    Bug fixed

--- lexer.py
+++ lexer.py
@@ -1,33 +1,66 @@
+from globalTypes import *
+
+def globales(programa, posicion, progLong):
+    global s
+    global p
+    global lng
+
+    s = programa
+    p = posicion
+    lng = progLong
+
 def reset(lexem, token):
     print(lexem, token)
     return '', 0
 
 def getToken(imprime=True):
     table = [
-        [1, 3, 12, 18, 13, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 19, 0],
-        [1]+[2]*17,
-        [0]*18,
-        [4, 3]+[4]*16,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18,
-        [0]*18
+        [1, 3, 12, 18, 13, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 19, 20, 0],
+        [1]+[2]*19,
+        [0]*20,
+        [4, 3]+[4]*18,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [21]*11+[22]+[21]*8,
+        [23]*11+[24]+[23]*8,
+        [25]*11+[26]+[25]*8,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [27]*19+[28],
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [0]*20,
+        [0]*20
     ]
-    s = 'int gcd (int u ,int v ){ }$'
+    reserved = {
+        'else': TokenType.ELSE,
+        'if': TokenType.IF,
+        'int': TokenType.INT,
+        'return': TokenType.RETURN,
+        'void': TokenType.VOID,
+        'while': TokenType.WHILE
+    }
+    # s = '1+1$'
+    # s = 'int gcd(int u, int v){ <=,,,, }$'
+    s = ' >=== >= hh 7 void !=$'
+
+    # s = open('test.c', 'r')
+    # s = s.read() + '$'     # lee todo el archivo a compilar
+
     blank = ' \n\t$'
     digit = '0123456789'
     letter = 'acbdefghijklmnopqrstuvwxyz'+'acbdefghijklmnopqrstuvwxyz'.upper()
@@ -36,13 +69,15 @@ def getToken(imprime=True):
     p = 0
     lexem = ''
     token = ''
+
+    tokens = []
     # import ipdb; ipdb.set_trace()
     while (s[p] != '$' or (s[p] == '$' and state != 0)):
         char = s[p]
         if char in letter:
             col = 0
         elif char in blank:
-            col = 17
+            col = 18
         elif char in digit:
             col = 1
         elif char == '[':
@@ -75,80 +110,111 @@ def getToken(imprime=True):
             col = 15
         elif char == ';':
             col = 16
+        elif char == '!':
+            col = 17
 
         state = table[state][col]
 
         if state == 2:
-            token = 'ID'
+            p -= 1
+            if lexem in reserved:
+                token = reserved[lexem]
+            else:
+                token = TokenType.ID
             lexem, state = reset(lexem, token)
         elif state == 4:
-            token = 'NUM'
+            p -= 1
+            token = TokenType.NUM
             lexem, state = reset(lexem, token)
         elif state == 5:
-            token = 'PLUS'
+            token = TokenType.PLUS
             lexem = '+'
             lexem, state = reset(lexem, token)
         elif state == 6:
-            token = 'MINUS'
+            token = TokenType.MINUS
             lexem = '-'
             lexem, state = reset(lexem, token)
         elif state == 7:
-            token = 'PLUS'
+            token = TokenType.TIMES
             lexem = '*'
             lexem, state = reset(lexem, token)
         elif state == 8:
-            token = 'DIV'
+            token = TokenType.DIV
             lexem = '/'
             lexem, state = reset(lexem, token)
-        elif state == 9:
-            token = 'LESSTHAN'
-            lexem = '<'
-            lexem, state = reset(lexem, token)
-        elif state == 10:
-            token = 'GREATERTHAN'
-            lexem += '>'
-            lexem, state = reset(lexem, token)
-        elif state == 11:
-            token = 'EQUALS'
-            lexem += '='
-            lexem, state = reset(lexem, token)
         elif state == 12:
-            token = 'OPENBRACKET'
+            token = TokenType.OPENBRACKET
             lexem += '['
             lexem, state = reset(lexem, token)
         elif state == 13:
-            token = 'CLOSEBRACKET'
+            token = TokenType.CLOSEBRACKET
             lexem = ']'
             lexem, state = reset(lexem, token)
         elif state == 14:
-            token = 'OPENPAR'
+            token = TokenType.OPENPAR
             lexem = '('
             lexem, state = reset(lexem, token)
         elif state == 15:
-            token = 'CLOSEPAR'
+            token = TokenType.CLOSEBRACKET
             lexem = ')'
             lexem, state = reset(lexem, token)
         elif state == 16:
-            token = 'OPENCURLY'
+            token = TokenType.OPENCURLY
             lexem = '{'
             lexem, state = reset(lexem, token)
         elif state == 17:
-            token = 'CLOSECURLY'
+            token = TokenType.CLOSECURLY
             lexem = '}'
             lexem, state = reset(lexem, token)
         elif state == 18:
-            token = 'COMA'
+            token = TokenType.COMMA
             lexem = ','
             lexem, state = reset(lexem, token)
         elif state == 19:
-            token = 'SEMICOLON'
+            token = TokenType.SEMICOLON
             lexem = ';'
+            lexem, state = reset(lexem, token)
+        elif state == 21:
+            token = TokenType.LESSTHAN
+            lexem = '<'
+            lexem, state = reset(lexem, token)
+        elif state == 22:
+            token = TokenType.LEQ
+            lexem = '<='
+            lexem, state = reset(lexem, token)
+        elif state == 23:
+            token = TokenType.GREATERTHAN
+            lexem = '>'
+            lexem, state = reset(lexem, token)
+        elif state == 24:
+            token = TokenType.GEQ
+            lexem = '>='
+            lexem, state = reset(lexem, token)
+        elif state == 25:
+            token = TokenType.EQUALS
+            lexem = '='
+            lexem, state = reset(lexem, token)
+        elif state == 26:
+            token = TokenType.EQEQ
+            lexem = '=='
+            lexem, state = reset(lexem, token)
+        elif state == 27:
+            token = TokenType.ERROR
+            lexem = '!'
+            lexem, state = reset(lexem, token)
+        elif state == 28:
+            token = TokenType.DIFF
+            lexem = '!='
+            lexem, state = reset(lexem, token)
+        else:
+            token = TokenType.ERROR
             lexem, state = reset(lexem, token)
         p += 1
 
-        if char == '$':
-            p -= 1
         if state != 0:
             lexem += char
+        else:
+            tokens.append(token)
+    return tokens + [TokenType.ENDFILE]
 
 getToken()
